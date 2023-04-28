edd89d36d683018d1ec4cde1bf5ad74003de037a
Christian Massaro <c.massaro@kiwibit.it>
2023-04-28 14:34:30 +0200

    status bar color for pwa

--- src/app/core/services/theme.service.ts
+++ src/app/core/services/theme.service.ts
@@ -16,6 +16,22 @@ export class ThemeService {
     this.document.body.classList.remove('default', 'kiwi-dark', 'bh');
     localStorage.setItem('omniak-theme', newTheme);
     this.document.body.classList.add(newTheme);
+    
+    /* Change PWA status bar color */
+    const metaThemeColor = document.querySelector("meta[name=theme-color]") as Element;
+    switch (newTheme) {
+      case 'default':
+        metaThemeColor.setAttribute("content", "#102c3d");
+        break;
+      case 'kiwi-dark':
+        metaThemeColor.setAttribute("content", "#242424");
+        break;
+      case 'bh':
+        metaThemeColor.setAttribute("content", "#042620");
+        break;
+      default:
+        break;
+    }
   }
 
 }
