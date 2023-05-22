ce7fe0bd97c13c337c2a14152962f2f73bc5eb3b
Christian Massaro <c.massaro@kiwibit.it>
2023-05-22 17:31:02 +0200

    Added camera upload

--- src/app/features/prediction/components/agenda/agenda.component.ts
+++ src/app/features/prediction/components/agenda/agenda.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AgendaComponent {
 
+  src: string | null | ArrayBuffer = "";
+
+  readURL(event: any): void {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.src = reader.result;
+
+      reader.readAsDataURL(file);
+    }
+  }
+
 }
