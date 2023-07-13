5e4e1aefa4dfd21e3a422b874fc25fb86c3039a7
Christian Massaro <c.massaro@kiwibit.it>
2023-07-13 17:31:22 +0200

    test for new version check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { filter } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { THEME_KEY, Theme, ThemeService } from './core/services/theme.service';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
-    private themeService: ThemeService
+    private themeService: ThemeService,
+    private swUpdate: SwUpdate
   ) {
     const theme = localStorage.getItem(THEME_KEY) as Theme | null;
     this.themeService.setTheme(theme ?? 'default');
+  }
+
+  ngOnInit(): void {
+    this.swUpdate.versionUpdates
+      .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+      .subscribe(evt => {
+        if (confirm()) {
+          // Reload the page to update to the latest version.
+          document.location.reload();
+        }
+      });
   }
 }
