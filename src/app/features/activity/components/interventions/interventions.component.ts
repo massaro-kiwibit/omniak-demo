ac49bf4b5c23bb5a710cd8dba50f116302fd9687
Christian Massaro <c.massaro@kiwibit.it>
2023-10-06 11:03:43 +0200

    minor fix

--- src/app/features/activity/components/interventions/interventions.component.ts
+++ src/app/features/activity/components/interventions/interventions.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { MbscDatepickerOptions } from '@mobiscroll/angular';
 import { PlatformService } from 'src/app/core/services/platform.service';
 import { Time } from 'src/app/shared/components/time-picker/time-picker.component';
@@ -14,7 +14,7 @@ export class InterventionsComponent {
   platform = inject(PlatformService);
   isMobile$ = this.platform.isMobile$;
 
-  time = null;
+  time: string | null = null;
 
   timeControl = new FormControl(new Time(null, null))
 
@@ -25,8 +25,14 @@ export class InterventionsComponent {
     console.log(this.time);
   }
 
-  options: MbscDatepickerOptions  = {
+  options: MbscDatepickerOptions = {
     baseTheme: 'material'
+  }
+
+  prova() {
+    const hours = this.time?.split('H')[0];
+    const minutes = this.time?.split('H')[1].replace('M', '');
+    return `${hours} ore ${minutes} minuti`
   }
 
 }
